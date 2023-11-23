702a9a2d6e55c715850cbb5374a7a6cc043a32c0
lipaysamart <lipaysamart@gmail.com>
2023-11-23 22:13:29 +0800

    Update

--- database/databasetup.go
+++ database/databasetup.go
@@ -1,1 +1,37 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func DBSet() *mongo.Client {
+	ClientOpts := options.Client().ApplyURI(
+		"mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), ClientOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb")
+		return nil
+	}
+	fmt.Println("Successfully Connected to the mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var productcollection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
+	return productcollection
+}
